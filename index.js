8a17ce31ffeb6a70a9b3a288abdc6f832ed4cdec
Hari Priya <haripriya@Haris-MacBook-Air.local>
2025-07-25 17:38:23 +0530

    changes are made on operations

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 //document.getElementById("count").innerText = 6;
 let count=9;
 console.log(count);
+let age=41;
+let myage=age;
+console.log(myage);
+let Dogage=myage*76;
+console.log(Dogage);
+let ordinary=5;
+console.log(ordinary);
+let doc1=ordinary+4;
+console.log(doc1);
+let doc2=ordinary*7;
+console.log(doc2);
